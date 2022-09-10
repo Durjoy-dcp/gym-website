fe551b2a69484a76f05a7d6ede6ffe47280dbafe
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-10 17:18:24 +0600

    switchTab()

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function changeTab(id) {
+    const contents = document.getElementsByClassName('body-contents');
+    for (content of contents) {
+        content.style.display = "none";
+    }
+    document.getElementById(id).style.display = "block";
+}
